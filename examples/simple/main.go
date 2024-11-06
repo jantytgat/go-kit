ddf4123ba9647c44bb9940da3fa9b039f6f86ac5
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-11-07 00:55:05 +0100

    Update sample application "simple"
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,34 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/spf13/cobra"
 
 	"github.com/jantytgat/go-kit/pkg/application"
+	"github.com/jantytgat/go-kit/pkg/semver"
+	"github.com/jantytgat/go-kit/pkg/slogd"
 )
 
-var rootCmd = &cobra.Command{
-	Use: "example",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println(cmd.UsageString())
-		return nil
-	},
+var testCmd = &cobra.Command{
+	Use:  "test",
+	RunE: testFunc,
 }
-var banner = "\nExample app\n-----------\n"
+
+func testFunc(cmd *cobra.Command, args []string) error {
+	out, mux := application.Output()
+	mux.Lock()
+	defer mux.Unlock()
+	if _, err := fmt.Fprintln(out, "test"); err != nil {
+		return err
+	}
+	slogd.FromContext(cmd.Context()).Log(cmd.Context(), slogd.LevelWarn.Level(), "test")
+	return nil
+}
 
 func main() {
 	var err error
-	var app *application.Application
+
+	slogd.Init(slogd.LevelTrace.Level(), false)
+	slogd.RegisterColoredTextHandler(os.Stderr, true)
+	slogd.RegisterTextHandler(os.Stderr, false)
+	slogd.RegisterJSONHandler(os.Stderr, false)
 
 	// You should typically set this using build flags
-	application.Version = "0.1.0-alpha.0+metadata.20101112"
-
-	if app, err = application.New(rootCmd, application.WithBanner(banner)); err != nil {
+	var version semver.Version
+	if version, err = semver.Parse("0.1.0-alpha.0+metadata.20101112"); err != nil {
 		panic(err)
 	}
 
-	if err = app.Run(); err != nil {
-		panic(err)
+	application.New("example", "Example App", "", version)
+	application.RegisterCommand(testCmd)
+	ctx := slogd.WithContext(context.Background())
+
+	if err = application.Run(ctx); err != nil {
+		slogd.Logger().LogAttrs(ctx, slogd.LevelError.Level(), "error running application", slog.Any("error", err))
 	}
+	// slogd.Logger().LogAttrs(ctx, slogd.LevelDebug.Level(), "test coloured")
+	// slogd.UseHandler(slogd.HandlerText)
+	// slogd.Logger().LogAttrs(ctx, slogd.LevelDebug.Level(), "test after change to text")
+	// slogd.SetLevel(slogd.LevelInfo)
+	// slogd.UseHandler(slogd.HandlerJSON)
+	// slogd.Logger().Log(ctx, slogd.LevelInfo.Level(), "test after level change to json")
+	// slogd.UseHandler(slogd.HandlerColor)
+	// slogd.Logger().Log(ctx, slogd.LevelInfo.Level(), "test after level change")
 }
