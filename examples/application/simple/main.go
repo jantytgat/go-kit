1167f290d528ccc3f745cc872bd995c7a52ee329
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-08-28 17:42:50 +0200

    Fix example application
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- examples/application/simple/main.go
+++ examples/application/simple/main.go
@@ -14,45 +14,16 @@ import (
 	"github.com/jantytgat/go-kit/slogd"
 )
 
-var (
-	version    string = "0.1.0-alpha.0+metadata.20101112"
-	branch     string = "0.1.0-dev"
-	tag        string = "0.1.0-dev.0"
-	commit     string = "aabbccddee"
-	commitDate string = time.Now().String()
-	buildDate  string = time.Now().String()
-
-	major      string = "0"
-	minor      string = "1"
-	patch      string = "0"
-	prerelease string = "dev"
-)
-
 func main() {
 	var err error
 	slogd.Init(application.GetLogLevelFromArgs(os.Args), false)
 	slogd.RegisterSink(slogd.HandlerText, slog.NewTextHandler(os.Stdout, slogd.HandlerOptions()), true)
 	ctx := slogd.WithContext(context.Background())
 
-	config := application.Config{
+	builder := application.Builder{
 		Name:   "main",
 		Title:  "Main Test",
 		Banner: "",
-		// Version:                "0.1.0-alpha.0+metadata.20101112",
-		Version: application.Version{
-			Full:       version,
-			Branch:     branch,
-			Tag:        tag,
-			Commit:     commit,
-			CommitDate: commitDate,
-			BuildDate:  buildDate,
-			Major:      major,
-			Minor:      minor,
-			Patch:      patch,
-			PreRelease: prerelease,
-		},
-		EnableGracefulShutdown: true,
-		Logger:                 slogd.Logger(),
 		OverrideRunE: func(cmd *cobra.Command, args []string) error {
 			mux := http.NewServeMux()
 			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -60,16 +31,18 @@ func main() {
 			})
 			return httpd.RunHttpServer(cmd.Context(), slogd.Logger(), "127.0.0.1", 28000, mux, 5*time.Second)
 		},
-		PersistentPreRunE:        nil,
-		PersistentPostRunE:       nil,
-		ShutdownSignals:          application.DefaultShutdownSignals,
-		ShutdownTimeout:          5 * time.Second,
-		SubCommands:              nil,
-		SubCommandInitializeFunc: nil,
-		ValidArgs:                nil,
+		PersistentPreRunE:  nil,
+		PersistentPostRunE: nil,
+		SubCommands:        nil,
+		ValidArgs:          nil,
+	}
+
+	var cmd *cobra.Command
+	if cmd, err = builder.Build(); err != nil {
+		panic(err)
 	}
 	var app application.Application
-	if app, err = application.New(config); err != nil {
+	if app, err = application.New(cmd, application.NewDefaultQuitter(application.DefaultShutdownTimeout), slogd.Logger()); err != nil {
 		panic(err)
 	}
 
